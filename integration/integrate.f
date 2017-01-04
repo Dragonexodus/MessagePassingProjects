83bbc4dbb704a313884d1c8ee56800a8d2114a74
Dragonexodus <d.ragonslayer@web.de>
2017-01-04 16:52:05 +0100

    added time, hcube, small impro

--- integration/integrate.f
+++ integration/integrate.f
@@ -1,34 +1,56 @@
        program integration
-       integer makering, links(0:1), size, topid, id, sum
-       double precision integrateF1
-       n = 10000
+       double precision integrateF1, integrateF2, micro
+       integer makehcube, dim, topid, id, start, stop
+       parameter(dim = 3, micro=10**6)
+       integer links(dim), timenowhigh, timediff
+
+c       integer iArg
+c       character cArg
+
+c       iArg = iargc()
+c       call getarg(iArg,cArg)
+c       print*,"iArg: " ,iArg
+c       print*,"cArg: " ,cArg
+
+c      Für Messungen wird 2^n empfohlen
+       n = 2**15
+
        size = nprocs ()
-       topid = makering (1, size, -1, -1, -1, -1, -1, -1, id, links)
-       sum = 0
+       topid = makehcube(1, dim, -1, -1, -1, -1, -1, -1, id, links)
+
 c TODO: read command line flag of used function
+
+
        if(id.eq.0.and.mod(n,2).eq.0) then
-              print*,"integral for f1: " ,integrateF1(n)                       
+              start = timenowhigh()
+              print*,"ID:",id," Pi f1:" ,integrateF1(n)
+              stop = timenowhigh()
+              print*,"ID:",id," T:", dble(timediff(stop,start)/micro)                     
+       endif
+       if(id.eq.1.and.mod(n,2).eq.0) then    
+              start = timenowhigh()
+              print*,"ID:",id," Pi f2:" ,integrateF2(n)  
+              stop = timenowhigh()
+              print*,"ID:",id," T: ", dble(timediff(stop,start)/micro)                   
        endif
 
        call freetop (topid)
        end
 
        double precision function integrateF1(n)      
-              integer n
-              double precision  a,b,h,f1, summe
-              a = 0
-              b = 3.14159265358979323
+              integer n,exponent
+              double precision  a,b,h,summe,step,f1
+              parameter ( a = 0, b = 3.14159265358979323)
+
               h= dble((b-a)/n)
 
               summe = f1(a)
+              step = a + h
               if(n.gt.2) then
                      do i=1,(n-2)
-                            if(mod(i,2).eq.0) then
-                                 summe = summe + 2*f1(a)
-                            else
-                                 summe = summe + 4*f1(a)
-                            endif
-                            a = a+h    
+                            exponent = mod(i,2) + 1 
+                            summe = summe + f1(step) * 2**exponent
+                            step = step + h    
                      enddo
               endif
               summe = summe + f1(b)
@@ -44,21 +66,18 @@ c TODO: read command line flag of used function
        end
 
        double precision function integrateF2(n)      
-              integer n
-              double precision  a,b,h,f2, summe
-              a = 0
-              b = 1
+              integer n,exponent
+              double precision  a,b,h,summe,step,f2
+              parameter ( a = 0, b = 1)
               h= dble((b-a)/n)
 
               summe = f2(a)
+              step = a + h
               if(n.gt.2) then
                      do i=1,(n-2)
-                            if(mod(i,2).eq.0) then
-                                 summe = summe + 2*f2(a)
-                            else
-                                 summe = summe + 4*f2(a)
-                            endif
-                            a = a+h    
+                            exponent = mod(i,2) + 1 
+                            summe = summe + f2(step) * 2**exponent
+                            step = step + h   
                      enddo
               endif
               summe = summe + f2(b)
